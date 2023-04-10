1dd33e230444911dce1b68036e1b186690a98d5a
Vaidotas Piekus <vp@Vaidotass-MacBook-Pro.local>
2023-04-10 20:24:55 +0200

    open url

--- lua/gitlab-open/init.lua
+++ lua/gitlab-open/init.lua
@@ -6,9 +6,11 @@ end
 
 -- Create a command, ':DoTheThing'
 vim.api.nvim_create_user_command(
-    'GLOpen',
+    'GLopen',
     function(input)
         print "Something should happen here..."
+        local url = "https://www.google.com"
+        vim.cmd('exec "!open" .. url')
     end,
     {bang = true, desc = 'a new command to do the thing'}
 )
