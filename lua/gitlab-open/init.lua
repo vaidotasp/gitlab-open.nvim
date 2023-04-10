84665baebd28feb3cf539bbcf12b41615327541a
Vaidotas Piekus <vp@Vaidotass-MacBook-Pro.local>
2023-04-10 20:15:52 +0200

    cmd

--- lua/gitlab-open/init.lua
+++ lua/gitlab-open/init.lua
@@ -1,7 +1,17 @@
 print("plugin loaded");
 
+-- TODO: accept params (GL/GH)
 local function setup(params)
 end
+
+-- Create a command, ':DoTheThing'
+vim.api.nvim_create_user_command(
+    'GLOpen',
+    function(input)
+        print "Something should happen here..."
+    end,
+    {bang = true, desc = 'a new command to do the thing'}
+)
 
 return {
 	setup = setup
