a42bf9b354152f4b0f711a1daa08d69eb9e3393c
Vaidotas Piekus <vaidotasp@gmail.com>
2023-04-11 07:39:10 +0200

    branch name

--- lua/gitlab-open/init.lua
+++ lua/gitlab-open/init.lua
@@ -1,4 +1,5 @@
 print("plugin loaded");
+local repoURL = "https://gitlab.com/remote-com/employ-starbase/dragon/blob/"
 
 -- TODO: accept params in the future. For
 -- example GitHub or Gitlab
@@ -7,8 +8,7 @@ end
 
 local url_opener_command = "!open"
 
--- TODO: Check if we are in a file or a scratch buffer, if buf,
--- exit
+-- TODO: Check if we are in a file or a scratch buffer, if buf -> exit
 local function get_current_file_path()
     local path = vim.fn.expand('%')
     return path
@@ -19,27 +19,40 @@ local function get_current_buffer_line()
     return line
 end
 
+-- TODO implement ranges
 local function get_visual_range()
     local    line1 = vim.api.nvim_buf_get_mark(0, "<")[1]
     local line2 = vim.api.nvim_buf_get_mark(0, ">")[1]
     return { line1, line2 }
 end
 
+local function get_git_branch()
+  local branch = vim.fn.systemlist('git symbolic-ref --short HEAD')[1]
+  return branch
+end
+
+local function getURL(file, line)
+    return repoURL .. get_git_branch() .. "/" .. file .. "#L" .. line
+end
+
+
+
 vim.api.nvim_create_user_command(
     'GLopen',
     function(input)
         -- print "Something should happen here..."
-        local url = "https://www.google.com"
+        -- local url = "https://www.google.com"
         -- vim.cmd('exec "!open https://www.google.com"')
 
-        local cmd = 'silent exec "' .. url_opener_command .. ' \'' .. url .. '\'"'
 
         local file = get_current_file_path()
         local line = get_current_buffer_line()
-        local vis = get_visual_range()
-        print(vis.line1)
+        -- local vis = get_visual_range()
+        local url = getURL(file, line)
+        print(url)
         --open the URL
-        -- vim.cmd(cmd)
+        local cmd = 'silent exec "' .. url_opener_command .. ' \'' .. url .. '\'"'
+        vim.cmd(cmd)
     end,
     { bang = true, desc = 'a new command to do the thing' }
 )
